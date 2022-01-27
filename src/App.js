97e8ff3879bfe1a08780495f8a31240f6adcb387
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-01-27 16:04:03 -0500

    create the sidebar menu component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Sidebar from './components/Sidebar/Sidebar.jsx';
 import Topbar from './components/Topbar/Topbar.jsx';
+import './app.css';
+import Home from './pages/Home/Home.jsx';
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className='container'>
+        <Sidebar />
+        <Home />
+      </div>
     </div>
   );
 }
