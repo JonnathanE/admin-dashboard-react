fe511a8f66fd77d017c2ac6ec8e6546164813a0a
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-01-28 13:24:01 -0500

    create chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Chart from '../../components/Chart/Chart';
 import FeaturedInfo from '../../components/FeatureInfo/FeaturedInfo';
 import './home.css';
+
+import { userData } from '../../data';
+;
+
 
 const Home = () => {
     return (
         <div className='home'>
             <FeaturedInfo />
+            <Chart title='User Analytics' data={userData} dataKey='Active User' grid />
         </div>
     );
 };
